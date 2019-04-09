7dc615f939d0bdc04b1a1169d0afe9e22e2f0573
Munish Chandel <cancerian0684@gmail.com>
2019-04-09 17:02:20 +0530

    Added JVM method calls from python flask app

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+import json
+
+import requests
+from flask import Flask, jsonify, Response
 
 app = Flask(__name__)
 
@@ -13,5 +16,20 @@ def health():
     return jsonify({"status": "UP"}), 200
 
 
+@app.route("/check", methods=["GET"])
+def check():
+    r = requests.get("http://localhost:5678/hosts/sidecar-pdf")
+    print(json.loads(r.text))
+    js = json.dumps(json.loads(r.text))
+    return Response(js, status=200, mimetype='application/json')
+
+
+@app.route("/invoke-slug", methods=["GET"])
+def invoke_slug():
+    r = requests.get("http://localhost:5678/slug/this-is-a-sample-url-a5s6f8f9")
+    # return Response(r.text, status=200, mimetype='application/json')
+    return jsonify(r.json())
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=8058)
